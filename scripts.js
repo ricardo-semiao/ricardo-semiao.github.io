e262a9f229febc5fccb0c20761efa508f05a3013
ricardo-semiao <ricardo.semiao@outlook.com>
2024-06-08 19:21:42 -0300

    Changed classes for ids, refactored navbar and footer to external files

--- scripts.js
+++
@@ -1,6 +0,0 @@
-var navLinks = document.getElementsByClassName('nav-link');
-for (var i = 0; i < navLinks.length; i++) {
-    if (navLinks[i].href == document.URL) {
-        navLinks[i].classList.add('current-page');
-    }
-}
---
+++ site_structure.js
@@ -0,0 +1,19 @@
+fetch('site_structure.html')
+    .then(response => response.text())
+    .then(data => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+
+        const headerHTML = doc.querySelector('#site-header').outerHTML;
+        const footerHTML = doc.querySelector('#site-footer').outerHTML;
+
+        document.querySelector('#site-header-container').innerHTML = headerHTML;
+        document.querySelector('#site-footer-container').innerHTML = footerHTML;
+    });
+
+var navLinks = document.getElementsByClassName('nav-link');
+for (var i = 0; i < navLinks.length; i++) {
+    if (navLinks[i].href == document.URL) {
+        navLinks[i].classList.add('current-page');
+    }
+}
