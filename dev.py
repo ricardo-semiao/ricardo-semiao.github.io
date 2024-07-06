9a6088e3c3d5298c196380b3cb9ba6584643d059
ricardo-semiao <ricardo.semiao@outlook.com>
2024-07-06 18:12:13 -0300

    Generalized aesthetic update

--- dev.py
+++ dev.py
@@ -1,8 +1,15 @@
 from template_injector import build
+import requests
 
 # Install with:
 #pip install git+https://github.com/ricardo-semiao/ricardo-semiao.github.io.git#subdirectory=template_injector
 
+# Downloading personal palette from github:
+response = requests.get("https://raw.githubusercontent.com/ricardo-semiao/ricardo-semiao/main/palette/palette.css?token=$(date%20+%s)")
+with open("site_assets/palette.css", 'wb') as file:
+    file.write(response.content)
+
+# Building the templates:
 build(
     'assets/home/home_template.html',
     'site_assets/components.html',
