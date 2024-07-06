fa9696165a73d8ac3313828603b3164386a13d08
ricardo-semiao <ricardo.semiao@outlook.com>
2024-07-06 20:46:11 -0300

    Structure: updated js to match subpages in navbar

--- site_assets/site_structure.js
+++ site_assets/site_structure.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var navLinks = document.getElementsByClassName('site-nav-link');
-    for (var i = 0; i < navLinks.length; i++) {
-        if (new URL(navLinks[i].href).pathname == new URL(document.URL).pathname) {
-            navLinks[i].classList.add('current-page');
+    var navlinks = document.getElementsByClassName('site-nav-link');
+    for (var i = 0; i < navlinks.length; i++) {
+        if (document.URL.split('/')[3] == navlinks[i].href.split('/')[3]) {
+            navlinks[i].classList.add('current-page');
         }
     }
 });
