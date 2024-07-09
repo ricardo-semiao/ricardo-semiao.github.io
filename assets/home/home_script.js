0a4aa24974bd0b868f363ec3d596fa002531da99
ricardo-semiao <ricardo.semiao@outlook.com>
2024-07-09 16:53:42 -0300

    Home: solved flickering on explanation cards

--- assets/home/home_script.js
+++ assets/home/home_script.js
@@ -9,13 +9,19 @@ document.addEventListener('DOMContentLoaded', function() {
             linkTextP.textContent = e.target.textContent
             explanationTextP.textContent = e.target.getAttribute('data-explanation');
 
-            explanationCard.style.display = 'block';
-            explanationCard.style.left = e.pageX + 'px';
+            explanationCard.style.position = 'absolute';
+            explanationCard.style.left = link.getBoundingClientRect().right + 6 + 'px';
             explanationCard.style.top = e.pageY - explanationCard.offsetHeight/2 + 'px' ;
+            explanationCard.style.visibility = 'visible';
         });
 
         link.addEventListener('mouseleave', function() {
-            explanationCard.style.display = 'none';
+            explanationCard.style.visibility = 'hidden';
         });
     });
 });
+
+// Assuming 'link' is the element you're positioning the explanationCard relative to
+const linkRect = link.getBoundingClientRect();
+explanationCard.style.position = 'absolute';
+explanationCard.style.left = linkRect.left + 4 + 'px';
