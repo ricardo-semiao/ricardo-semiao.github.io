92e13c788e6a989f515aac9b05696fcaf6eeda24
ricardo-semiao <ricardo.semiao@outlook.com>
2024-08-02 14:52:31 -0300

    Reorganized structure for sticky footer

--- site_assets/site_structure.js
+++ site_assets/site_structure.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var navlinks = document.getElementsByClassName('site-nav-link');
+    var navlinks = document.querySelectorAll('.nav-li a');
     for (var i = 0; i < navlinks.length; i++) {
         if (document.URL.split('/')[3] == navlinks[i].href.split('/')[3]) {
             navlinks[i].classList.add('current-page');
