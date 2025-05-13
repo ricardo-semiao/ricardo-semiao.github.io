92c1da46dc962489abf5fd0b984bcafd014d2d6c
ricardo-semiao <ricardo.semiao@outlook.com>
2025-05-13 18:38:35 -0300

    Updated projects and cv (added html-based pdf), navbar color and weight changes

--- site_assets/site_structure.js
+++ site_assets/site_structure.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < navlinks.length; i++) {
         if (document.URL.split('/')[3] == navlinks[i].href.split('/')[3]) {
             navlinks[i].classList.add('current-page');
+            if (navlinks[i].closest('.nav-drop')) {
+                navlinks[i].closest('.nav-drop').querySelector('p').classList.add('current-page');
+            }
         }
     }
 });
